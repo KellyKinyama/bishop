88e5302c3177a614fa4a0aab575dc11da58074f9
kelikovic <kkinyama@zesco.co.zm>
2025-09-24 12:04:50 +0200

    jjb

--- lib/src/gpt_value_model.dart
+++ lib/src/gpt_value_model.dart
@@ -1,12 +1,9 @@
 // gpt_model.dart
-
-// import 'dart:math';
-// import 'package:bishop/bishop.dart';
 import 'package:dart_torch/transformer/transformer_decoder.dart';
 import 'package:dart_torch/nn/value.dart';
 import 'package:dart_torch/nn/value_vector.dart';
+// ... (imports and existing classes like SGD)
 
-// Re-using a simple SGD optimizer
 class SGD {
   final List<Value> parameters;
   final double learningRate;
@@ -99,7 +96,6 @@ class GptModel {
         'd1d8'
       ],
     ];
-
     final Map<String, int> stoi = _generateChessVocab(rawGameSequences);
     final Map<int, String> itos =
         stoi.map((key, value) => MapEntry(value, key));
@@ -108,7 +104,6 @@ class GptModel {
     const int blockSize = 16;
     const int numLayers = 2;
     const int numHeads = 4;
-
     final gptModel = TransformerDecoder(
       vocabSize: vocabSize,
       embedSize: embedSize,
@@ -130,7 +125,8 @@ class GptModel {
     );
   }
 
-  Future<void> trainOnSequence(List<String> gameMoves) async {
+  Future<void> trainOnSequence(
+      List<String> gameMoves, double gameResult,) async {
     final List<int> tokenizedSeq = [
       startTokenId,
       ...gameMoves.map((move) => stoi[move]!),
@@ -140,38 +136,52 @@ class GptModel {
     List<int> input = tokenizedSeq.sublist(0, tokenizedSeq.length - 1);
     List<int> target = tokenizedSeq.sublist(1);
 
-    if (input.length > blockSize) {
+      if (input.length > blockSize) {
       input = input.sublist(0, blockSize);
       target = target.sublist(0, blockSize);
     }
 
+
     optimizer.zeroGrad();
 
-    final List<ValueVector> logits = decoder.forward(input, []);
+    // Call the model and get both the policy and value outputs
+    final List<ValueVector> outputs = decoder.forward(input, []);
+    final ValueVector policyLogits = outputs[0];
+    final ValueVector valueOutput = outputs[1];
 
-    Value batchLoss = Value(0.0);
+    // 1. Calculate Policy Loss (Cross-Entropy)
+    Value policyLoss = Value(0.0);
     int activeTokens = 0;
-    for (int t = 0; t < logits.length; t++) {
-      final ValueVector tokenLogits = logits[t];
+    for (int t = 0; t < policyLogits.values.length; t++) {
+      final ValueVector tokenLogits = policyLogits; // Assuming single output
       final int trueTargetId = target[t];
       final Value trueLogit = tokenLogits.values[trueTargetId];
       final Value sumExpLogits =
           tokenLogits.values.map((v) => v.exp()).reduce((a, b) => a + b);
       final Value logSumExp = sumExpLogits.log();
       final Value negLogProb = logSumExp - trueLogit;
-      batchLoss += negLogProb;
+      policyLoss += negLogProb;
       activeTokens++;
     }
 
     if (activeTokens > 0) {
-      batchLoss = batchLoss / Value(activeTokens.toDouble());
-    } else {
-      batchLoss = Value(0.0);
+      policyLoss = policyLoss / Value(activeTokens.toDouble());
     }
 
-    batchLoss.backward();
+    // 2. Calculate Value Loss (Mean Squared Error)
+    // The target value is the final game result (-1 for loss, 0 for draw, 1 for win)
+    final Value predictedValue = valueOutput.values.first;
+    final Value targetValue = Value(gameResult);
+    final Value valueLoss = (predictedValue - targetValue).pow(2);
+
+    // 3. Combine losses
+    // You can use a weight to balance the two losses, e.g., 0.5 for each
+    final Value totalLoss = policyLoss + valueLoss;
+
+    totalLoss.backward();
     optimizer.step();
 
-    print('Trained on ${gameMoves.length} moves. Loss: ${batchLoss.data}');
+    print(
+        'Trained on ${gameMoves.length} moves. Total Loss: ${totalLoss.data}');
   }
 }
